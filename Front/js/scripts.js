e6d16060ddd3fd81557124693bd9bb076fcea234
Gabriel <gabrielsribeiro9999@gmail.com>
2023-07-23 12:56:45 -0300

    as

--- Front/js/scripts.js
+++ Front/js/scripts.js
@@ -1,2 +1,31 @@
 
-function a
+function verificaCor(){
+    let cor = document.getElementById('cor').value;
+    cor = cor.toLowerCase()
+    switch (cor) {
+        case "azul":
+            // a
+            document.body.style.backgroundColor = 'blue';
+            break;
+
+        case "vermelho":
+            // a
+            document.body.style.backgroundColor = 'red';
+            break;
+
+        case "preto":
+            // a
+            document.body.style.backgroundColor = 'black';
+            break;
+
+        case "amarelo":
+            // a
+            document.body.style.backgroundColor = 'yellow';
+            break;
+        default:
+            document.getElementById('text').innerHTML = 'Não foi encerido nehuma cor';
+            document.body.style.backgroundColor = 'yellow';
+    }
+
+
+}
