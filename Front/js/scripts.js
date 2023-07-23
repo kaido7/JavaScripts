48c884b036f7c5e8f5fe1b44e1e04636d8826809
Gabriel <gabrielsribeiro9999@gmail.com>
2023-07-23 10:59:16 -0300

    a

--- Front/js/scripts.js
+++ Front/js/scripts.js
@@ -1,10 +1,2 @@
 
-
-var items_1 = "Arroz";
-var items_2 = "Feijão";
-var items_3 = "Macarrão";
-var items_4 = "Salada";
-
-const lista = ["Arroz", "Feijão",  "Macarrão", "Salada"];
-lista.unshift("Carne", "Mamão")
-console.log(lista);
+function a
