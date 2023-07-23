3da71cddc1f5b710792dcc2a3787ec2b09d018ab
Gabriel <gabrielsribeiro9999@gmail.com>
2023-07-23 18:55:44 -0300

    for

--- Front/js/scripts.js
+++ Front/js/scripts.js
@@ -1,31 +1,3 @@
-
-function verificaCor(){
-    let cor = document.getElementById('cor').value;
-    cor = cor.toLowerCase()
-    switch (cor) {
-        case "azul":
-            // a
-            document.body.style.backgroundColor = 'blue';
-            break;
-
-        case "vermelho":
-            // a
-            document.body.style.backgroundColor = 'red';
-            break;
-
-        case "preto":
-            // a
-            document.body.style.backgroundColor = 'black';
-            break;
-
-        case "amarelo":
-            // a
-            document.body.style.backgroundColor = 'yellow';
-            break;
-        default:
-            document.getElementById('text').innerHTML = 'Não foi encerido nehuma cor';
-            document.body.style.backgroundColor = 'yellow';
-    }
-
-
+for (let i = 0; i < 999; i++){
+    document.getElementById('text').innerHTML = i
 }
