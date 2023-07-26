deab8c8b07e1ca10d81d0f76fa6975ca64887ca4
Gabriel <gabrielsribeiro9999@gmail.com>
2023-07-26 20:16:43 -0300

    Time

--- Front/js/scripts.js
+++ Front/js/scripts.js
@@ -1,8 +1,28 @@
 
 
 function ativarContagem(){
-    document.getElementById('tempo').innerHTML = 'Começou'
-    setTimeout(function(){
-        document.getElementById('tempo').innerHTML = 'Executado o tempo'
-    }, 5000);
+    
+    // document.getElementById('tempo').innerHTML = 'Começou a Contagem';
+    
+    // tempo = setTimeout(function(){
+        
+    //     document.getElementById('tempo').innerHTML = 'Executando o Tempo';
+    
+    // }, 5000);
+
+    setInterval(function(){
+        var tmp = document.getElementById('tempo').innerHTML;
+        var soma = parseInt(tmp) + 1;
+        document.getElementById('tempo').innerHTML = soma;
+    }, 1000
+    );
+
+}
+
+
+
+function pararContagem(){
+    clearTimeout(tempo);
+    document.getElementById('tempo').innerHTML = 'Parou a Contagem';
+
 }
