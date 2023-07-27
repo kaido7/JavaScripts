9c57c552596d04d1641ac80114da427c58e1e808
Gabriel <gabrielsribeiro9999@gmail.com>
2023-07-27 18:10:51 -0300

    console

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,6 +1,13 @@
 
-const carro = {
-    marca: 'Fiat',
-    modelo: 'Uno',
-    ano: 1998,
-}
+class Carro{
+    constructor(valor_1, valor_2, valor_3){
+        this.marca = valor_1
+        this.modelo = valor_2
+        this.ano = valor_3
+    };
+};
+
+
+
+console.log( new Carro('Fiat', 'Uno', 1999));
+console.log( new Carro('Palio', 'flex', 2010));
