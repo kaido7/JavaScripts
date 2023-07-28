681172834d77850f09752ce1d73c5d32872381d1
Gabriel <gabrielsribeiro9999@gmail.com>
2023-07-28 20:01:00 -0300

    atual

--- JS/js/script.js
+++ JS/js/script.js
@@ -5,9 +5,13 @@ class Carro{
         this.modelo = valor_2
         this.ano = valor_3
     };
+
+    buzina(){
+        return this.modelo + ' Buzinou: Biiiiiii';
+    }
 };
 
 
 
 console.log( new Carro('Fiat', 'Uno', 1999));
-console.log( new Carro('Palio', 'flex', 2010));
+console.log( new Carro('Fokswag', 'Gol', 2010).buzina());
